7ce98a691087b7c948c2e326be21b0358258539c
Viorel Robu <wwjd.vio@gmail.com>
2020-10-03 14:54:01 +0300

    search and run js test files

---
+++ runner.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+const forbiddenDirs = ['node_modules'];
+
+class Runner {
+    constructor()
+    {
+        this.testFiles = [];
+    }
+    async runTests() {
+        for (let file of this.testFiles) {
+            console.log(chalk.gray(`---- ${file.shortName} ----`));
+            const beforeEaches = [];
+            global.beforeEach = (fn) => {
+                beforeEaches.push(fn);
+            };
+
+            global.it = (desc, fn) => {
+                beforeEaches.forEach(func => func());
+                try {
+                    fn();
+                    console.log(`${chalk.green('Pass')} - ${desc}`);
+                } catch (error) {
+                    const message = error.message.replace(/\n/g, '\n\t\t');
+                    console.log(`${chalk.red('Fail')} - ${desc}`)
+                    console.log('\t', message);
+                }
+            };
+
+            try {
+                require(file.name);
+            } catch (error) {
+                console.log(chalk.red('X - Error Loading File', file.name));
+                console.log(error);
+            }
+        }
+    }
+
+    async collectFiles(targetPath) 
+    {
+        const files = await fs.promises.readdir(targetPath);
+        for (let file of files) {
+            const filepath = path.join(targetPath, file);
+            const stats = await fs.promises.lstat(filepath);
+
+            if (stats.isFile() && file.includes('.test.js')) {
+                this.testFiles.push({ 
+                    name: filepath,
+                    shortName: file
+                });
+            } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
+                const childFiles = await fs.promises.readdir(filepath);
+                files.push(...childFiles.map( f => path.join(file, f)));
+            }
+        }
+    }
+}
+
+module.exports = Runner;
