cf5c501d88e95bafb8663c6a31c8df7e41d4e957
Viorel Robu <wwjd.vio@gmail.com>
2020-10-07 12:49:57 +0300

    added sample projects

---
+++ sampleproject/test/forEach.test.js
@@ -0,0 +1,27 @@
+const {forEach} = require('../index');
+const assert = require('assert');
+
+let numbers;
+beforeEach(() => {
+    numbers = [1, 2, 3];
+});
+
+it('Should sum an array', () => {
+    let total = 0;
+    forEach(numbers, (value) => {
+        total += value;
+    });
+
+    assert.strictEqual(total, 6);
+    numbers.push(3);
+    numbers.push(3);
+    numbers.push(3);
+    numbers.push(3);
+    numbers.push(3);
+    numbers.push(3);
+    numbers.push(3);
+});
+
+it('beforeEach is ran each time', () => {
+    assert.strictEqual(numbers.length, 4);
+});
